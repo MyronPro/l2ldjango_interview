6985cb697706def33f64d47e8dc1d123747fc202
MyronPro <myroncp@yahoo.com>
2021-08-25 18:11:10 -0500

    Added additional error handling to l2l_extras filter

--- l2l/templatetags/l2l_extras.py
+++ l2l/templatetags/l2l_extras.py
@@ -5,13 +5,21 @@ register=template.Library()
 
 @register.filter(name='l2l_dt')
 def l2l_dt(val):
+    try:
+        # convert strings to datetime obj
+        if isinstance(val, str):
+            try:
+                # string expected format: %Y-%m-%dT%H:%M:%S
+                val = datetime.datetime.strptime(val, '%Y-%m-%dT%H:%M:%S')
+            except ValueError:
+                return '{INVALID_STRING_FORMAT}'
 
-    # convert strings to datetime obj
-    if isinstance(val, str):
-        try:
-            # string expected format: %Y-%m-%dT%H:%M:%S
-            val = datetime.datetime.strptime(val, '%Y-%m-%dT%H:%M:%S')
-        except ValueError:
-            return '{INVALID_STRING_FORMAT}'
+        #return datetime obj in %Y-%m-%d %H:%M:%S format
+        return val.strftime('%Y-%m-%d %H:%M:%S')
 
-    return val.strftime('%Y-%m-%d %H:%M:%S')
+    except AttributeError:
+        return '{INVALID_DATA_TYPE}'
+
+    except Exception as e:
+        print(e)
+        return '{ERROR}'
